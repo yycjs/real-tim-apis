78542d2d2a3c2519187718cd6016c0d89280e787
David Luecke <daff@neyeon.com>
2015-04-07 16:26:30 -0600

    Working API that doesn't look pretty.

--- public/app.js
+++ public/app.js
@@ -1,5 +1,33 @@
 import $ from 'jquery';
+import _ from 'lodash';
+
+const socket = io.connect();
+
+function updatePoll() {
+  socket.emit('polls::get', 1, {}, (error, poll) => {
+    let choices = _.sortBy(poll.choices, 'votes').reverse();
+    let list = $('<ul>');
+    choices.forEach(choice => {
+      let li = $(`<li>
+        ${choice.name} (${choice.votes}) <a class="upvote">Upvote</a>
+      </li>`);
+      li.data('choice', choice);
+      list.append(li);
+    });
+    $('#main').html(list);
+  });
+}
+
+socket.on('votes updated', updatePoll);
 
 $(() => {
-  console.log('Here');
+  $(document).on('click', 'li .upvote', function() {
+    let choice = $(this).parent().data('choice');
+
+    socket.emit('votes::update', choice.href, { votes: choice.votes + 1 }, {}, () => {
+      console.log('Updated vote');
+    });
+  });
+
+  updatePoll();
 });
