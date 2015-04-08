cd8d9214854316f0adf67516aa62389450277081
David Luecke <daff@neyeon.com>
2015-04-07 18:36:24 -0600

    Finalizing some stuff

--- public/app.js
+++ public/app.js
@@ -4,17 +4,21 @@ import _ from 'lodash';
 const socket = io.connect();
 
 function updatePoll() {
+  $('.upvote').hide();
   socket.emit('polls::get', 1, {}, (error, poll) => {
     let choices = _.sortBy(poll.choices, 'votes').reverse();
-    let list = $('<ul>');
+    let list = $('<ul class="list-group">');
     choices.forEach(choice => {
-      let li = $(`<li>
-        ${choice.name} (${choice.votes}) <a class="upvote">Upvote</a>
+      let li = $(`<li class="list-group-item">
+        <span class="glyphicon glyphicon-triangle-top upvote"></span>
+        ${choice.name}
+        <span class="badge">${choice.votes}</span>
       </li>`);
       li.data('choice', choice);
       list.append(li);
     });
     $('#main').html(list);
+    $('.upvote').show();
   });
 }
 
